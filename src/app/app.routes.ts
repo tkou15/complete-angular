8eebdf52deb06fad9d0ffbfb5320443e8ba120e3
Koichi Takahashi <tkou15.hi@gmail.com>
2025-02-17 00:48:22 +0900

    add style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/Home/home.component';
+import { StyleGuidesComponent } from './pages/style-guides/style-guides.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'style-guides', component: StyleGuidesComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
